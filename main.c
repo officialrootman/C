c5a9792e6eac455b6400e3245bfb82fe53424c27
officialrootman <muhammetcanaltan41@gmail.com>
2025-04-13 01:40:16 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,91 +5,104 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <signal.h>
 #include <time.h>
 
-#define PORT 8080       // Dinlenecek port numarası (değiştirilebilir)
+#define PORT 8080       // Dinlenecek port numarası (gerektiğinde değiştirilebilir)
 #define BUFFER_SIZE 1024
 
-// Gelen bağlantı bilgilerini log dosyasına kaydeden fonksiyon
-void log_connection(struct sockaddr_in *client_addr) {
-    FILE *fp = fopen("honeypot.log", "a");
-    if (fp == NULL) {
-        perror("Log dosyası açılamadı");
-        return;
-    }
-    // Şu anki zamanı al
-    time_t now = time(NULL);
-    char *timestr = ctime(&now);
-    // Yeni satır karakterini kaldır
-    if (timestr[strlen(timestr)-1] == '\n')
-        timestr[strlen(timestr)-1] = '\0';
+// Programın düzgün kapanması için global bayrak
+volatile sig_atomic_t running = 1;
 
-    fprintf(fp, "Bağlantı: %s - Zaman: %s\n", inet_ntoa(client_addr->sin_addr), timestr);
-    fclose(fp);
+// SIGINT yakalayıcı: Ctrl+C ile çalışmayı sonlandırıyoruz.
+void handle_sigint(int signum) {
+    running = 0;
+    printf("\nSIGINT alındı. Honeypot kapatılıyor...\n");
 }
 
-int main(int argc, char *argv[]) {
+int main() {
     int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
+    struct sockaddr_in address, client_addr;
     int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE] = {0};
-    const char *message = "Honeypot'a hoşgeldiniz!\n";
 
-    // 1. Socket oluşturulması
+    // SIGINT (Ctrl+C) sinyalini yakala
+    signal(SIGINT, handle_sigint);
+
+    // 1. Socket oluşturulması (TCP)
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket oluşturulamadı");
+        perror("Socket oluşturulurken hata oluştu");
         exit(EXIT_FAILURE);
     }
 
-    // 2. Socket seçeneklerinin ayarlanması: Adresin yeniden kullanılabilir olması
+    // 2. Socket seçenekleri: Adresin tekrar kullanılabilir olması
+    int opt = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("setsockopt hatası");
         exit(EXIT_FAILURE);
     }
 
-    // 3. Adres yapılandırması
+    // 3. Adres ve port yapılandırması
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // 4. Bind işlemi: Socket'i belirlenen IP ve port'a bağlama
+    // 4. Bind: Socket'i belirtilen IP ve port'a bağlama
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Bind hatası");
+        perror("Bind işlemi başarısız");
         exit(EXIT_FAILURE);
     }
-    printf("Honeypot %d portu üzerinde dinleniyor...\n", PORT);
 
-    // 5. Dinleme: Gelen bağlantıları bekleme
+    printf("Honeypot port %d üzerinde çalışıyor. Bağlantılar bekleniyor...\n", PORT);
+
+    // 5. Dinleme: Gelen bağlantıları kabul etme
     if (listen(server_fd, 3) < 0) {
-        perror("Listen hatası");
+        perror("Dinleme sırasında hata");
         exit(EXIT_FAILURE);
     }
 
-    // 6. Sürekli olarak gelen bağlantıları kabul etme
-    while (1) {
-        struct sockaddr_in client_addr;
+    // 6. Sonsuz döngü: Gelen bağlantıları kabul et ve terminale yazdır
+    while(running) {
         socklen_t client_addr_len = sizeof(client_addr);
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len)) < 0) {
-            perror("Accept hatası");
+        new_socket = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (new_socket < 0) {
+            // Eğer running bayrağı sıfırlanmışsa (Ctrl+C) döngüden çık.
+            if (!running)
+                break;
+            perror("Bağlantı kabul edilemedi");
             continue;
         }
 
-        // Gelen bağlantı loglanıyor
-        log_connection(&client_addr);
+        // Bağlantı anındaki zamanı al ve formatla
+        time_t now = time(NULL);
+        char time_str[26];
+        ctime_r(&now, time_str);
+        // Yeni satır karakterini kaldır
+        size_t len = strlen(time_str);
+        if (len > 0 && time_str[len - 1] == '\n')
+            time_str[len - 1] = '\0';
 
-        // Saldırganın dikkatini çekmek için hoşgeldiniz mesajı gönderiliyor
-        send(new_socket, message, strlen(message), 0);
+        // Terminale bağlantı bilgisini yazdır
+        printf("\n[%s] Bağlantı: %s\n", time_str, inet_ntoa(client_addr.sin_addr));
 
-        // İsteğe bağlı olarak, gönderilen veriyi okuyabiliriz
+        // İstemciye hoşgeldiniz mesajı gönder
+        const char *welcome_msg = "Honeypot'a hoş geldiniz!\n";
+        send(new_socket, welcome_msg, strlen(welcome_msg), 0);
+
+        // İstemciden gelen veriyi oku
+        char buffer[BUFFER_SIZE] = {0};
         int bytes_read = read(new_socket, buffer, BUFFER_SIZE);
         if (bytes_read > 0) {
-            printf("Gelen veri: %s\n", buffer);
+            printf("Alınan veri: %s\n", buffer);
+        } else if (bytes_read < 0) {
+            perror("Veri okunurken hata oluştu");
         }
 
-        // Bağlantı kapatılıyor
+        // Bağlantıyı kapat
         close(new_socket);
     }
 
+    // Server socket'ini kapat ve çık
+    close(server_fd);
+    printf("Honeypot kapatıldı.\n");
     return 0;
 }
